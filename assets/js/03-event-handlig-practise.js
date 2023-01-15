4e219e3cce8e5708993edc9d54c9e494a9c447ee
Nida Yucedal <yucedalnida@gmail.com>
2023-01-15 16:03:41 +0300

    event

--- assets/js/03-event-handlig-practise.js
+++ assets/js/03-event-handlig-practise.js
@@ -1,7 +1,8 @@
 const btnAverage=document.querySelector("#btnAverage");
+
 btnAverage.addEventListener("click", ()=> {
-const point1=document.querySelector("txtPoint1").value;
-const point2=document.querySelector("txtPoint2").value;
+const point1=document.querySelector("#txtPoint1").value;
+const point2=document.querySelector("#txtPoint2").value;
 
 const average=getAverage(point1,point2);
 console.log(average);
@@ -9,6 +10,10 @@ console.log(average);
 const letter= convertNumberToLetter(average);
 alert(letter);
 });
+
+// Number(x) -> x degerini numeric tipe cevirir.Ancak boslukalri sıfır olarak kabul eder
+// parseFloat(x) -> x degerini decimal (ondalikli) tipe cevirir.Boslluklar NaN olarak kabul edilir
+// parseInt(x) -> x degerini integer (tamsayi) tipine cevirir. Boslluklar NaN olarak kabul edilir
 
 const getAverage = (point1,point2) =>{
     //dısaridan 2 deger aliyor
@@ -23,7 +28,26 @@ const getAverage = (point1,point2) =>{
 }
 
 const convertNumberToLetter = (num) => {
-    //islemler
+   if(isNaN(parseFloat(num))) return;
 
-    return "C";
+   let letter;
+   if(num>=90 && num<=100){
+    letter="A";
+   }
+   else if (num >= 80 && num<90){
+    letter="B";
+   }
+   else if (num >= 70 && num<80){
+    letter="C";
+   }
+   else if (num >= 50 && num<70){
+    letter="D";
+   }
+   else if (num >= 0 && num<50){
+    letter="F";
+   }
+   
+
+
+    return letter;
 }
