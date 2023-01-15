e2b83d62f2a3e108363197c2bb54ddb8d2132896
Nida Yucedal <yucedalnida@gmail.com>
2023-01-15 15:34:31 +0300

    event

--- assets/js/03-event-handlig-practise.js
+++ assets/js/03-event-handlig-practise.js
@@ -1,4 +1,29 @@
 const btnAverage=document.querySelector("#btnAverage");
 btnAverage.addEventListener("click", ()=> {
-    
-})
+const point1=document.querySelector("txtPoint1").value;
+const point2=document.querySelector("txtPoint2").value;
+
+const average=getAverage(point1,point2);
+console.log(average);
+
+const letter= convertNumberToLetter(average);
+alert(letter);
+});
+
+const getAverage = (point1,point2) =>{
+    //dısaridan 2 deger aliyor
+    //kontrolunu yaptiik
+    if(isNaN(parseFloat(point1))|| isNaN(parseFloat(point2))) return;
+
+    const average=(parseFloat(point1) + parseFloat(point2))/2;
+
+    return average;
+
+
+}
+
+const convertNumberToLetter = (num) => {
+    //islemler
+
+    return "C";
+}
