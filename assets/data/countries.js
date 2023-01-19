14361c2be40e0178a7552c4e47658f37f754f1dc
Nida Yucedal <yucedalnida@gmail.com>
2023-01-19 20:13:52 +0300

    iteration

---
+++ assets/data/countries.js
@@ -0,0 +1,18 @@
+export const countries=[
+    {
+    "name": {
+        "common":"Aruba",
+        "official":"Aruba",
+        "native":{
+            "nld":{
+                "official":"Aruba",
+                "common":"Aruba",
+
+            },
+            "pap":{
+                "official":"Aruba",
+                "common":"Aruba"
+            },
+        },
+    },
+    },];
---
+++ assets/js/for-of-practise.js
@@ -0,0 +1,1 @@
+import { countries } from "../data/countries";
