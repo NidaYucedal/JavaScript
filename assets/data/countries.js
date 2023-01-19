b474b0e202c3c8c62f74bc76d515b5a396c676df
Nida Yucedal <yucedalnida@gmail.com>
2023-01-19 20:48:00 +0300

    iteration

--- assets/data/countries.js
+++ assets/data/countries.js
@@ -15,4 +15,59 @@ export const countries=[
             },
         },
     },
-    },];
+    },
+
+    {
+        "name": {
+            "common":"America",
+            "official":"Aruba",
+            "native":{
+                "nld":{
+                    "official":"Aruba",
+                    "common":"Aruba",
+    
+                },
+                "pap":{
+                    "official":"Aruba",
+                    "common":"Aruba"
+                },
+            },
+        },
+        },
+    
+        {
+            "name": {
+                "common":"Canada",
+                "official":"Aruba",
+                "native":{
+                    "nld":{
+                        "official":"Aruba",
+                        "common":"Aruba",
+        
+                    },
+                    "pap":{
+                        "official":"Aruba",
+                        "common":"Aruba"
+                    },
+                },
+            },
+            },
+        
+        
+            {
+                "name": {
+                    "common":"Japan",
+                    "official":"Aruba",
+                    "native":{
+                        "nld":{
+                            "official":"Aruba",
+                            "common":"Aruba",
+            
+                        },
+                        "pap":{
+                            "official":"Aruba",
+                            "common":"Aruba"
+                        },
+                    },
+                },
+                },];
