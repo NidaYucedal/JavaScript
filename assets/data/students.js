a54737240f938e4c53f2082f9e2c871e9a6a3ff5
Nida Yucedal <yucedalnida@gmail.com>
2023-01-20 15:31:57 +0300

    new

--- assets/data/students.js
+++ assets/data/students.js
@@ -1,6 +1,6 @@
 export const students =[
-    {id:1,name:"Ayse",puan:80},
-    {id:2,name:"Veli",puan:56},
-    {id:3,name:"Fatma",puan:95},
-    {id:4,name:"Asya",puan:65},
+    {id:1,name:"Ayse",point:80},
+    {id:2,name:"Veli",point:56},
+    {id:3,name:"Fatma",point:95},
+    {id:4,name:"Asya",point:65},
 ]
--- assets/js/dom-practise.js
+++ assets/js/dom-practise.js
@@ -1,1 +1,27 @@
 import {students} from "../data/students.js"
+
+const loadStudents = () => { 
+
+    let strStudents="";
+
+    students.forEach((student,index)=>{
+        strStudents+=`
+        <tr>
+        <th scope="row">${index+1}</th>
+        <td>${student.name}</td>
+        <td>${student.point}</td>
+        </tr>`;
+    });
+
+    document.querySelector("#tblStudents tbody").innerHTML=strStudents;
+
+ };
+
+
+ const setStudentBg = () => {
+    const rows = document.querySelectorAll("#tblStudents tbody tr");
+
+
+    
+ }
+ loadStudents();
