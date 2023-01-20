f5aa2efd1cb109fe29bdff041c0e24e755dc9ed1
Nida Yucedal <yucedalnida@gmail.com>
2023-01-20 20:46:28 +0300

    dom

---
+++ assets/js/08-dom.js
@@ -0,0 +1,60 @@
+import {students} from "../data/students.js"
+
+const loadStudents = () => { 
+
+    let strStudents="";
+
+    students.forEach((student,index)=>{
+        strStudents+=`
+        <tr>
+        <th scope="row">${index + 1}</th>
+        <td>${student.name}</td>
+        <td>${student.point}</td>
+        <td><button class="btn btn-danger btn-delete">Del</button></td>
+        </tr>`;
+    });
+
+    document.querySelector("#tblStudents tbody").innerHTML=strStudents;
+
+ };
+
+
+ const setStudentBg = () => {
+    const rows = document.querySelectorAll("#tblStudents tbody tr");
+
+
+    for(let row of rows){
+        const point=row.querySelector("td:nth-child(3)").innerText;
+        if(point<50){
+            row.classList.add("table-danger")
+        }
+    }
+ }
+
+
+ loadStudents();
+ setStudentBg();
+ 
+ document.querySelectorAll(".btn-delete").forEach((btn) => {
+    btn.addEventListener("click", (e)=> {
+        const row=e.target.closest("tr");
+       const name=row.querySelector("td").innerText;
+      
+
+      
+       const result= confirm (`${name} isimli kaydi silmek istediginizden emin misiniz?`);
+
+       if(!result) return;
+
+       row.remove();
+   
+    });
+
+});
+
+    document.querySelectorAll("#tblStudents tbody tr").forEach((row)=>{
+        row.addEventListener("click",(e)=>{
+            console.log(e.target);
+            e.target.closest("tr").classList.toggle("table-warning");
+        });
+    });
