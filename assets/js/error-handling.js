2ce43351d37dd7d79be6d6c85ebe79e8995ee6fe
Nida Yucedal <yucedalnida@gmail.com>
2023-01-21 17:50:09 +0300

    error

--- assets/js/error-handling.js
+++ assets/js/error-handling.js
@@ -3,11 +3,30 @@ document.querySelector("#frmLogin").addEventListener("submit", () => {
     e.preventDefault();
 //kullanicidan bilgi alirken veya bir API dan bilgi
 //cekerken kullanilir
+      
+       const email=document.querySelector("#email").value;
+       const password=document.querySelector("#password").value;
 
-    try {
-        
-    } catch (error) {
-        
-    }
-    alert("submit yapildi");
+       try {
+
+       if(!isEmail(email)) throw "Lutfen gecerli bir eposta giriniz";
+
+       if(!password) throw "Lutfen sifrenizi giriiniz";
+
+       e.target.submit();
+
+       }
+       catch (error)
+     { alert(error);
+       }
+
 });
+
+
+const isEmail = (email) => { 
+
+    //email kontrol islemleri
+
+    return true;
+
+ };
