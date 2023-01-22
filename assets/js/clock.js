a0b91c492a506e29d89cb6e3464805b8fa665520
Nida Yucedal <yucedalnida@gmail.com>
2023-01-22 14:58:07 +0300

    clock

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -2,3 +2,13 @@
 const elhour = document.querySelector("#clock li:nth-child(1)");
 const elMinute = document.querySelector("#clock li:nth-child(3)");
 const elSecond = document.querySelector("#clock li:nth-child(2)");
+
+
+const tick = () => { 
+
+
+ }
+
+
+
+ setInterval(tick,1000);
