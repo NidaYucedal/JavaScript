7342317580d504f9000ae802c26469eca1b30b10
Nida Yucedal <yucedalnida@gmail.com>
2023-01-24 14:20:24 +0300

    modules

--- assets/js/module.js
+++ assets/js/module.js
@@ -2,7 +2,7 @@
 ///*moduller,js kodlari bolerek farkli farkli dosyalardan cagirmak ve*/
 //boylece buyuk projelerde kod karmasasinin onune gecmek icin kullanilan bir yontem
 
-import { showAlert } from "./message"
+import { showAlert } from "./message.js"
 
   //export disari aktarma
   //import iceri alma
