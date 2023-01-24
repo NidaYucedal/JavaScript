9c78e1d4292a64657f78677ffc2504b1a004c480
Nida Yucedal <yucedalnida@gmail.com>
2023-01-24 17:42:33 +0300

    fetch

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -7,11 +7,25 @@ document.querySelector("#txtSearch").addEventListener("click",(e)=>{
     .then( (resp)=>resp.json())
      .then((data)=>{
 
-        console.log(data);
+       // console.log(data);
 
+       let strHtml="";
         data.forEach((item)=>{
             console.log(item);
+
+            strHtml += `
+            <div class="col">
+            <div class="card">
+              <img src="${item.show.image.medium}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              </div>
+            </div>
+          </div>`
      })
+
+     const elTvShows=document.querySelector("#tvShows").innerHTML = strHtml;
 
     });
    
