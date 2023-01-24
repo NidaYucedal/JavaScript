3200106f21ccca4e1671b5ac17d3ba3fff8d8b94
Nida Yucedal <yucedalnida@gmail.com>
2023-01-24 17:46:16 +0300

    fetch

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -16,10 +16,10 @@ document.querySelector("#txtSearch").addEventListener("click",(e)=>{
             strHtml += `
             <div class="col">
             <div class="card">
-              <img src="${item.show.image.medium}" class="card-img-top" alt="...">
+              <img src="${item.show.image.medium}" class="card-img-top" alt="${item.show.name}">
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${item.show.name}</h5>
+                <p class="card-text">${item.show.genres[0]}</p>
               </div>
             </div>
           </div>`
