4a3574d35b11e0b2dd75183192cae8900bec3a89
Nida Yucedal <yucedalnida@gmail.com>
2023-01-24 14:41:20 +0300

    modules

--- assets/js/module-practise.js
+++ assets/js/module-practise.js
@@ -8,8 +8,9 @@ const arr=[1,56,3,67,2,87,55,33];
 console.log(nSortDesc(arr));
 console.log(nSortAsc(arr));
 
-const arrNames=["ali","zeynep","sevgi","temel"];
+const arrNames=["ali","zeynep","şakir","ssevgi","temel"];
 
 console.log(sSortAsc(arrNames));
+console.log(sSortDesc(arrNames));
 
 
