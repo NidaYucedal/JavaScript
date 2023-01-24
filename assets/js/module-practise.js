c5d125c2d3c1a005fd4adb6f464b79ed7c467691
Nida Yucedal <yucedalnida@gmail.com>
2023-01-24 14:37:00 +0300

    modules

--- assets/js/module-practise.js
+++ assets/js/module-practise.js
@@ -1,4 +1,11 @@
 import {nSortDesc,nSortAsc,sSortAsc,sSortDesc} from "./sort.js"
 
 
-const arr=[1,56,3,67,2]
+const arr=[1,56,3,67,2,87,55,33];
+//const arr1=[1,56,3,67,2,87,55,33];
+
+
+console.log(nSortDesc(arr));
+console.log(nSortAsc(arr));
+
+
--- assets/js/sort.js
+++ assets/js/sort.js
@@ -1,23 +1,23 @@
 const sSortAsc=(arr)=>{
 
-    return arr.sort((a,b)=>a<b?-1:1);
+    return arr.sort((a,b) => a<b ? -1 : 1);
 
 };
 
 const sSortDesc=(arr)=>{
 
-    return arr.sort((a,b)=>b<a?-1:1);
+    return arr.sort((a,b) => b < a ? -1 : 1);
 
 };
 //[12,45,2,76,9]
 const nSortAsc=(arr)=>{
-    return arr.sort((a,b)=>a-b);
+    return arr.sort((a,b) => a-b);
 
 };
 
 const nSortDesc=(arr)=>{
 
-    return arr.sort((a,b)=>b-a);
+    return arr.sort((a,b) => b-a);
 
     
 }
