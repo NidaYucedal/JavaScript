5b2bd2017fa25dc0e67a9951e6ae846e89b3fc4f
Nida Yucedal <yucedalnida@gmail.com>
2023-01-24 14:43:30 +0300

    modules

--- assets/js/module-practise.js
+++ assets/js/module-practise.js
@@ -8,6 +8,9 @@ const arr=[1,56,3,67,2,87,55,33];
 console.log(nSortDesc(arr));
 console.log(nSortAsc(arr));
 
+console.log("şakir".localeCompare("ali"));
+//tarayicinin diline gore hareket ederr
+//sakir aliden buyuk 1 doner
 const arrNames=["ali","zeynep","şakir","ssevgi","temel"];
 
 console.log(sSortAsc(arrNames));
